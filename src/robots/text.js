bd480cb054264eac1542685d7fc472a999abb381
diglopes <diego-chez@hotmail.com>
2019-08-24 16:20:21 -0300

    feat: start text robot

---
+++ src/robots/text.js
@@ -0,0 +1,68 @@
+const algorithmia = require("algorithmia");
+const algorithmiaApiKey = require("../credentials/algorithmia.json").apiKey;
+const sentenceBoundaryDetection = require("sbd");
+
+async function robot(content) {
+  await fetchContentFromWikipedia(content);
+  saniteContent(content);
+  breakConentIntoSentences(content);
+
+  async function fetchContentFromWikipedia(content) {
+    const algorithmiaAunthenticated = algorithmia(algorithmiaApiKey);
+    const wikipediaAlgorithm = algorithmiaAunthenticated.algo(
+      "web/WikipediaParser/0.1.2?timeout=300"
+    );
+    const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm);
+    const wikipediaContent = wikipediaResponse.get();
+
+    content.sourceContentOriginal = wikipediaContent.content;
+  }
+
+  function saniteContent(content) {
+    const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(
+      content.sourceContentOriginal
+    );
+    const withoutDatesInParentheses = removeDatesInParentheses(
+      withoutBlankLinesAndMarkdown
+    );
+
+    content.sourceContentSanitized = withoutDatesInParentheses;
+
+    function removeBlankLinesAndMarkdown(text) {
+      const allLines = text.split("\n");
+
+      const withoutBlankLines = allLines.filter(line => {
+        if (line.trim().length === 0 || line.trim().startsWith("=")) {
+          return false;
+        }
+
+        return true;
+      });
+
+      return withoutBlankLines.join(" ");
+    }
+
+    function removeDatesInParentheses(text) {
+      return text
+        .replace(/\((?:\([^()]*\)|[^()])*\)/gm, "")
+        .replace(/  /g, " ");
+    }
+  }
+
+  function breakConentIntoSentences(content) {
+    content.sentences = [];
+
+    const sentences = sentenceBoundaryDetection.sentences(
+      content.sourceContentSanitized
+    );
+    sentences.forEach(sentence => {
+      content.sentences.push({
+        text: sentence,
+        keywords: [],
+        images: []
+      });
+    });
+  }
+}
+
+module.exports = robot;
